74d370c4e79776bdea4b0ad12c1fe2cb3d76d588
Matej Talirevic <talirevic.matej@gmail.com>
2024-07-18 17:10:06 +0200

    level design

---
+++ src/components/Level.tsx
@@ -0,0 +1,241 @@
+import * as THREE from 'three'
+import {CuboidCollider, RapierRigidBody, RigidBody} from "@react-three/rapier"
+import {ComponentType, useMemo, useRef, useState} from "react";
+import {useFrame} from "@react-three/fiber";
+import {useGLTF} from "@react-three/drei";
+
+const boxGeometry: THREE.BoxGeometry = new THREE.BoxGeometry(1, 1, 1)
+const floor1Material = new THREE.MeshStandardMaterial({color: 'limegreen'})
+const floor2Material = new THREE.MeshStandardMaterial({color: 'greenyellow'})
+const obstacleMaterial = new THREE.MeshStandardMaterial({color: 'orangered'})
+const wallMaterial = new THREE.MeshStandardMaterial({color: 'slategrey'})
+
+export const BlockStart = ({position = [0, 0, 0]}: { position?: [number, number, number] }) => {
+    return (
+        <group position={position}>
+            <mesh
+                geometry={boxGeometry}
+                material={floor1Material}
+                position={[0, -0.1, 0]}
+                scale={[4, 0.2, 4]}
+                receiveShadow
+            />
+        </group>
+    )
+}
+
+const BlockEnd = ({position = [0, 0, 0]}: { position?: [number, number, number] }) => {
+    const hamburger = useGLTF('./hamburger.glb')
+
+    hamburger.scene.children.forEach((mesh) => {
+        mesh.castShadow = true
+    })
+
+    return (
+        <group position={position}>
+            <mesh
+                geometry={boxGeometry}
+                material={floor1Material}
+                position={[0, 0, 0]}
+                scale={[4, 0.2, 4]}
+                receiveShadow
+            />
+            <RigidBody
+                type='fixed'
+                colliders='hull'
+                position={[0, 0.25, 0]}
+                restitution={0.2}
+                friction={0}
+            >
+                <primitive
+                    object={hamburger.scene}
+                    scale={0.2}
+                />
+            </RigidBody>
+        </group>
+    )
+}
+
+export const BlockSpinner = ({position = [0, 0, 0]}: { position?: [number, number, number] }) => {
+    const obstacle = useRef<RapierRigidBody>(null)
+    const [speed] = useState(() => (Math.random() + 0.2) * (Math.random() < 0.5 ? -1 : 1))
+
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime()
+        const rotation = new THREE.Quaternion()
+        rotation.setFromEuler(new THREE.Euler(0, time * speed, 0))
+        obstacle.current?.setNextKinematicRotation(rotation)
+    })
+
+    return (
+        <group position={position}>
+            <mesh
+                geometry={boxGeometry}
+                material={floor2Material}
+                position={[0, -0.1, 0]}
+                scale={[4, 0.2, 4]}
+                receiveShadow
+            />
+            <RigidBody
+                ref={obstacle}
+                type='kinematicPosition'
+                position={[0, 0.3, 0]}
+                restitution={0.2}
+                friction={0}
+            >
+                <mesh
+                    geometry={boxGeometry}
+                    material={obstacleMaterial}
+                    scale={[3.5, 0.3, 0.3]}
+                    castShadow
+                    receiveShadow
+                />
+            </RigidBody>
+        </group>
+    )
+}
+
+export const BlockLimbo = ({position = [0, 0, 0]}: { position?: [number, number, number] }) => {
+    const obstacle = useRef<RapierRigidBody>(null)
+    const [timeOffset] = useState(() => Math.random() * Math.PI * 2)
+
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime()
+        const y = Math.sin(time + timeOffset) + 1.15
+        obstacle.current?.setNextKinematicTranslation({x: position[0], y: position[1] + y, z: position[2]})
+    })
+
+    return (
+        <group position={position}>
+            <mesh
+                geometry={boxGeometry}
+                material={floor2Material}
+                position={[0, -0.1, 0]}
+                scale={[4, 0.2, 4]}
+                receiveShadow
+            />
+            <RigidBody
+                ref={obstacle}
+                type='kinematicPosition'
+                position={[0, 0.3, 0]}
+                restitution={0.2}
+                friction={0}
+            >
+                <mesh
+                    geometry={boxGeometry}
+                    material={obstacleMaterial}
+                    scale={[3.5, 0.3, 0.3]}
+                    castShadow
+                    receiveShadow
+                />
+            </RigidBody>
+        </group>
+    )
+}
+
+export const BlockAxe = ({position = [0, 0, 0]}: { position?: [number, number, number] }) => {
+    const obstacle = useRef<RapierRigidBody>(null)
+    const [timeOffset] = useState(() => Math.random() * Math.PI * 2)
+
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime()
+        const x = Math.sin(time + timeOffset) * 1.25
+        obstacle.current?.setNextKinematicTranslation({x: position[0] + x, y: position[1] + 0.75, z: position[2]})
+    })
+
+    return (
+        <group position={position}>
+            <mesh
+                geometry={boxGeometry}
+                material={floor2Material}
+                position={[0, -0.1, 0]}
+                scale={[4, 0.2, 4]}
+                receiveShadow
+            />
+            <RigidBody
+                ref={obstacle}
+                type='kinematicPosition'
+                position={[0, 0.3, 0]}
+                restitution={0.2}
+                friction={0}
+            >
+                <mesh
+                    geometry={boxGeometry}
+                    material={obstacleMaterial}
+                    scale={[1.5, 1.5, 0.3]}
+                    castShadow
+                    receiveShadow
+                />
+            </RigidBody>
+        </group>
+    )
+}
+
+const Bounds = ({length = 1}: { length?: number }) => {
+    return (
+        <>
+            <RigidBody
+                type='fixed'
+                restitution={0.2}
+                friction={0}
+            >
+                <mesh
+                    position={[2.15, 0.75, -(length * 2) + 2]}
+                    geometry={boxGeometry}
+                    material={wallMaterial}
+                    scale={[0.3, 1.5, 4 * length]}
+                    castShadow
+                />
+                <mesh
+                    position={[-2.15, 0.75, -(length * 2) + 2]}
+                    geometry={boxGeometry}
+                    material={wallMaterial}
+                    scale={[0.3, 1.5, 4 * length]}
+                    receiveShadow
+                />
+                <mesh
+                    position={[0, 0.75, -(length * 4) + 2]}
+                    geometry={boxGeometry}
+                    material={wallMaterial}
+                    scale={[4, 1.5, 0.3]}
+                    receiveShadow
+                />
+                <CuboidCollider
+                    args={[2, 0.1, 2 * length]}
+                    position={[0, -0.1, -(length * 2) + 2]}
+                    restitution={0.2}
+                    friction={1}
+                />
+            </RigidBody>
+        </>
+    )
+}
+
+interface LevelProps {
+    count?: number
+    types?: ComponentType<{ position?: [number, number, number] }>[]
+}
+
+export const Level = ({count = 5, types = [BlockSpinner, BlockAxe, BlockLimbo]}: LevelProps) => {
+    const blocks = useMemo(() => {
+            const blocks = []
+            for (let i = 0; i < count; i++) {
+                const type = types[Math.floor(Math.random() * types?.length)]
+                blocks.push(type)
+            }
+            return blocks
+        },
+        [count, types])
+    return (
+        <>
+            <BlockStart position={[0, 0, 0]}/>
+            {blocks.map((Block, i) =>
+                <Block
+                    key={i}
+                    position={[0, 0, -(i + 1) * 4]}
+                />)}
+            <BlockEnd position={[0, 0, -(count + 1) * 4]}/>
+            <Bounds length={count + 2}/>
+        </>
+    )
+}
